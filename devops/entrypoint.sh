0748eb7ca4d08407059040504634cd36987ce2f5
Alberto Garcia <alberto@agarcia.io>
2024-08-01 23:35:45 -0700

    Add Dockerfile(s)

---
+++ devops/entrypoint.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+python manage.py collectstatic --no-input --clear
+python manage.py migrate
+gunicorn reservation.wsgi --name henry-reservation --workers 3 --timeout 60 --bind 0.0.0.0:8000
+exec "$@"
