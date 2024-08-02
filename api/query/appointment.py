6448dc62d918b7c2501c525b4d49d8c876db5506
Alberto Garcia <alberto@agarcia.io>
2024-08-01 17:13:07 -0700

    Add availableAppointments + confirmedAppointments to root level schema

--- api/query/appointment.py
+++ api/query/appointment.py
@@ -13,6 +13,8 @@ class AppointmentType(graphene.ObjectType):
     time_slot = graphene.DateTime()
     provider = graphene.Field('api.query.provider.ProviderType')
 
+    client = graphene.Field('api.query.client.ClientType')
+
 
     @staticmethod
     def resolve_time_slot(root: Any, info: graphene.ResolveInfo) -> graphene.String:
@@ -22,6 +24,15 @@ class AppointmentType(graphene.ObjectType):
     @staticmethod
     def resolve_provider(root: Any, info: graphene.ResolveInfo) -> graphene.String:
         return info.context.loaders.provider.load(root.provider_id)
+
+
+    @staticmethod
+    def resolve_client(root: Any, info: graphene.ResolveInfo) -> graphene.String:
+
+        reservation = info.context.loaders.confirmed_reservation_from_appointment.load(root.id)
+        client = reservation.then(lambda res: info.context.loaders.client.load(res.client_id) if res else None)
+
+        return client
 
 
     @classmethod
@@ -46,7 +57,20 @@ class Query(graphene.ObjectType):
     appointment = graphene.Node.Field(AppointmentType)
     appointments = graphene.ConnectionField(AppointmentConnection)
 
+    available_appointments = graphene.ConnectionField(AppointmentConnection)
+    confirmed_appointments = graphene.ConnectionField(AppointmentConnection)
+
 
     @staticmethod
     def resolve_appointments(root: None, info: graphene.ResolveInfo, **kwargs) -> Iterable[Any]:
         return Appointment.objects.all()
+
+
+    @staticmethod
+    def resolve_available_appointments(root: None, info: graphene.ResolveInfo, **kwargs) -> Iterable[Any]:
+        return Appointment.available_objects.all()
+
+
+    @staticmethod
+    def resolve_confirmed_appointments(root: None, info: graphene.ResolveInfo, **kwargs) -> Iterable[Any]:
+        return Appointment.confirmed_objects.all()
