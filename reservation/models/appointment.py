2bb01e0d6b2fb8d8feab8fc2d823629013fa872f
Alberto Garcia <alberto@agarcia.io>
2024-08-01 17:12:06 -0700

    Add object managers to Appointment model

--- reservation/models/appointment.py
+++ reservation/models/appointment.py
@@ -1,7 +1,30 @@
 from django.db import models
+from .reservation import Reservation
+
+
+class AvailableAppointmentManager(models.Manager):
+
+    def get_queryset(self):
+
+        reservations = Reservation.confirmed_objects.all() | Reservation.reserved_objects.all()
+        appointment_ids = [r.appointment_id for r in reservations]
+
+        return super().get_queryset().exclude(id__in=appointment_ids)
+
+
+class ConfirmedAppointmentManager(models.Manager):
+
+    def get_queryset(self):
+
+        confirmed_appointment_ids = Reservation.confirmed_objects.all()
+
+        return super().get_queryset().filter(id__in=confirmed_appointment_ids)
 
 
 class Appointment(models.Model):
 
     time_slot = models.DateTimeField()
     provider = models.ForeignKey('reservation.Provider', on_delete=models.CASCADE)
+
+    available_appointments = AvailableAppointmentManager()
+    confirmed_appointments = ConfirmedAppointmentManager()
