5db8b2a300be53b6e38367c01d2e0e541aed79a6
Alberto Garcia <alberto@agarcia.io>
2024-08-14 01:13:13 -0700

    Flesh out tests.api.query.test_appointment

--- tests/api/query/test_appointment.py
+++ tests/api/query/test_appointment.py
@@ -5,7 +5,7 @@ import graphene
 
 from api.query.appointment import Query
 from tests.api.utils import connection_to_list, request_with_loaders
-from reservation.models import Appointment, Provider
+from reservation.models import Appointment, Provider, Reservation
 
 from examples.queries import AvailableAppointments
 
@@ -21,7 +21,33 @@ class TestAvailableAppointmentsQuery(TestCase):
         self.schema = graphene.Schema(query=Query)
         self.request = request_with_loaders()
 
-        provider = baker.make(Provider)
+        self.provider = baker.make(Provider)
+
+
+    def test_available_appointments_query(self):
+
+        appointments = []
+
+        self.now = timezone.now() + datetime.timedelta(hours=25)
+        for slot_num in range(10):
+
+            offset = slot_num * 15
+            time_slot = self.now + datetime.timedelta(minutes=offset)
+
+            appointment = Appointment(time_slot=time_slot, provider=self.provider)
+            appointments.append(appointment)
+
+        Appointment.objects.bulk_create(appointments)
+
+        result = self.schema.execute(AvailableAppointments, context=self.request)
+        self.assertIsNone(result.errors, msg='Errors prevented execution for %s' % AvailableAppointments)
+
+        available_appointments = connection_to_list(result.data['availableAppointments'])
+        self.assertTrue(len(available_appointments) == 10)
+
+
+    def test_available_appointments_some_expired_query(self):
+
         appointments = []
 
         self.now = timezone.now() + datetime.timedelta(hours=22, minutes=59)
@@ -30,13 +56,37 @@ class TestAvailableAppointmentsQuery(TestCase):
             offset = slot_num * 15
             time_slot = self.now + datetime.timedelta(minutes=offset)
 
-            appointment = Appointment(time_slot=time_slot, provider=provider)
+            appointment = Appointment(time_slot=time_slot, provider=self.provider)
             appointments.append(appointment)
 
         Appointment.objects.bulk_create(appointments)
 
+        result = self.schema.execute(AvailableAppointments, context=self.request)
+        self.assertIsNone(result.errors, msg='Errors prevented execution for %s' % AvailableAppointments)
 
-    def test_available_appointments_query(self):
+        available_appointments = connection_to_list(result.data['availableAppointments'])
+        self.assertTrue(len(available_appointments) == 5)
+
+
+    def test_available_appointments_some_reserved_query(self):
+
+        appointments = []
+
+        self.now = timezone.now() + datetime.timedelta(hours=25)
+        for slot_num in range(10):
+
+            offset = slot_num * 15
+            time_slot = self.now + datetime.timedelta(minutes=offset)
+
+            appointment = Appointment(time_slot=time_slot, provider=self.provider)
+            appointments.append(appointment)
+
+        appointment_objs = Appointment.objects.bulk_create(appointments)
+
+
+        for i in range(10):
+            if (i % 2) == 0:
+               baker.make(Reservation, confirmed=True, appointment=appointment_objs[i])
 
         result = self.schema.execute(AvailableAppointments, context=self.request)
         self.assertIsNone(result.errors, msg='Errors prevented execution for %s' % AvailableAppointments)
